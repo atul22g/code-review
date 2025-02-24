4e3c45ca845e0b9fb2b4a7d409b434dac1231a6d
Atul <atul22vcoders@gmaill.com>
2025-02-24 15:06:18 +0530

    remove env port

--- BackEnd/server.js
+++ BackEnd/server.js
@@ -1,8 +1,6 @@
 require('dotenv').config()
 const app = require('./src/app')
-const port = process.env.BackendPORT || 3000
 
-
-app.listen(port, () => {
-    console.log(`Server is running on http://localhost:${port}`)
+app.listen(3001, () => {
+    console.log(`Server is running on http://localhost:3001`)
 })
